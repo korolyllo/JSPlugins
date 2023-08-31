4d2f394b3dc5092f0fb7220fe11b33fa6345495f
korolyllo <55107892+korolyllo@users.noreply.github.com>
2023-08-31 11:10:08 +0300

    Update action.js

--- action.js
+++ action.js
@@ -3,12 +3,5 @@ console.warn("Congratulations!");
 */
 const message = 'hello world';
 
-const number = 10;
-
-function multiplyNumbers(a, b) {
-    return a * b;
-}
-
-// exporting variables and function
-export { message, number, multiplyNumbers };
+console.warn('Congratuletions');
 
